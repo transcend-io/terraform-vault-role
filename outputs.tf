0b5a06050aa3585cff5aecaf689d21a499af82c3
dmattia <david@transcend.io>
2021-04-21 10:48:35 -0500

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,28 +1,40 @@
-output vault_role_name {
+output vault_role_name_map {
   value = try(
     { for name, resource in vault_aws_auth_backend_role.role: name => resource.role },
     { for name, resource in vault_approle_auth_backend_role.approle_role: name => resource.role_name },
-    ""
+    {}
   )
-  description = "The name of the Vault Role created"
+  description = "The name of the Vault Role created, keyed by the backend name"
 }
 
-output approle_accessor {
-  value       = try(vault_approle_auth_backend_role_secret_id.approle_secret[0].accessor, "")
-  description = "ID for approle logins"
+output approle_accessor_map {
+  value       = try(
+    { for name, resource in vault_approle_auth_backend_role_secret_id.approle_secret: name => resource.accessor },
+    {}
+  )
+  description = "ID for approle logins, keyed by the backend name"
 }
 
 output approle_wrapping_accessor {
-  value       = try(vault_approle_auth_backend_role_secret_id.approle_secret[0].wrapping_accessor, "")
-  description = "The unique ID for the response-wrapped SecretID that can be safely logged."
+  value       = try(
+    { for name, resource in vault_approle_auth_backend_role_secret_id.approle_secret: name => resource.wrapping_accessor },
+    {}
+  )
+  description = "The unique ID for the response-wrapped SecretID that can be safely logged, keyed by the backend name."
 }
 
 output approle_wrapping_token {
-  value       = try(vault_approle_auth_backend_role_secret_id.approle_secret[0].wrapping_token, "")
-  description = "The token used to retrieve a response-wrapped SecretID."
+  value       = try(
+    { for name, resource in vault_approle_auth_backend_role_secret_id.approle_secret: name => resource.wrapping_token },
+    {}
+  )
+  description = "The token used to retrieve a response-wrapped SecretID, keyed by the backend name."
 }
 
 output approle_role_id {
-  value       = try(vault_approle_auth_backend_role.approle_role[0].role_id, "")
-  description = "ID of the approle created. This ID is auto-generated"
+  value       = try(
+    { for name, resource in vault_approle_auth_backend_role.approle_role: name => resource.role_id },
+    {}
+  )
+  description = "ID of the approle created. This ID is auto-generated, and is keyed by the backend name"
 }
