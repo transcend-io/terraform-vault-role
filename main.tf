6f0ed3503565aa782beb3171fb0c9e8134ddbcb9
dmattia <david@transcend.io>
2021-04-22 16:27:20 -0500

    fix role name

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "vault_approle_auth_backend_role_secret_id" "approle_secret" {
   for_each = vault_approle_auth_backend_role.approle_role
 
   backend      = each.value.backend
-  role_name    = "${each.value.role_name}_${var.region}"
+  role_name    = each.value.role_name
   wrapping_ttl = var.wrapping_ttl
 }
 
